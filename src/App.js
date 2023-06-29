fa659ecd8a9383a4129d4a2a75ec90d09d9ca738
Surajit <piklu3043@gmail.com>
2023-06-29 15:59:02 +0530

    styling add movie btn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Cards from "./components/Cards";
+import { Routes, Route } from "react-router-dom";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Cards />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/addmovie" element={<AddMovie />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,26 @@
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="text-3xl font-bold text-red-500 p-3 border-b-2 border-gray-500 flex justify-between items-center">
-      <span>
-        {" "}
-        Filmy<span className="text-white">Verse</span>
-      </span>
+      <Link to={"/"}>
+        <span>
+          {" "}
+          Filmy<span className="text-white">Verse</span>
+        </span>
+      </Link>
 
-      <h1 className="text-lg flex items-center cursor-pointer">
-        <Button>
-          <AddIcon color="secondary" className="mr-1" />
-          <span className="text-white ">Add New</span>
-        </Button>
-      </h1>
+      <Link to={"/addmovie"}>
+        <h1 className="text-lg flex items-center cursor-pointer">
+          <Button>
+            <AddIcon color="secondary" className="mr-1" />
+            <span className="text-white ">Add New</span>
+          </Button>
+        </h1>
+      </Link>
     </div>
   );
 };
